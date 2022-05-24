bafe5787bcbca2f2a2dc38019c2aa7572e3cfb79
FilipeMSouza <filipe.souza1906@gmail.com>
2022-05-23 23:16:34 -0300

    Minor Updates
    - Updating project font
    - Update on libraries

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Spline+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
         </Head>
         <body>
           <Main />
